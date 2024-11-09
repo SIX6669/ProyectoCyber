2cd848079d75e297ad04c610132481f3c00b3a98
SIX6669 <110596196+SIX6669@users.noreply.github.com>
2024-11-09 18:15:25 -0300

    x

--- controlador/AdministradorController.java
+++ controlador/AdministradorController.java
@@ -1,6 +1,34 @@
 package controlador;
+import modelo.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static BD.util.DBConnection.setConnection;
 
 public class AdministradorController {
-    
+    private static final String INSERT_USER_SQL = "INSERT INTO Users (id, username, password) VALUES (?, ?)";
+    private static final String SELECT_USER_BY_ID = "SELECT * FROM Users WHERE id = ?";
+    private static final String SELECT_ALL_USERS = "SELECT * FROM Users";
+    private static final String DELETE_USER_SQL = "DELETE FROM Users WHERE id = ?";
+    private static final String UPDATE_USER_SQL = "UPDATE Users SET username = ?, password = ? WHERE id = ?";
+
+    public void crearUsuario( Cliente cliente) throws SQLException {
+       try(Connection connection = setConnection();
+       PreparedStatement ps = connection.prepareStatement(INSERT_USER_SQL)) {
+           ps.setInt(1,cliente.getNroUsuario());
+           ps.setString(2,cliente.getNombre());
+           ps.setString(3, cliente.getApellido());
+           ps.setInt(5,cliente.getTelefono());
+           ps.executeUpdate();
+       } catch (RuntimeException e) {
+           throw new RuntimeException(e);
+       }
+    }
+
+    public static String getSelectUserById() {
+        return SELECT_USER_BY_ID;
+    }
 }
 
--- modelo/Cliente.java
+++ modelo/Cliente.java
@@ -1,5 +1,64 @@
 package modelo;
 
+import java.sql.*;
+
 public class Cliente {
-    
+    public int nroUsuario;
+    public String nombre;
+    public String apellido;
+    public int telefono;
+    public Time tiempoAdquirido;
+
+    public Cliente(int nroUsuario, String nombre, String apellido, int telefono, Time tiempoAdquirido) {
+        this.nroUsuario = nroUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.tiempoAdquirido = tiempoAdquirido;
+    }
+
+    public int getNroUsuario() {
+        return nroUsuario;
+    }
+
+    public void setNroUsuario(int nroUsuario) {
+        this.nroUsuario = nroUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Time getTiempoAdquirido() {
+        return tiempoAdquirido;
+    }
+
+    public void setTiempoAdquirido(Time tiempoAdquirido) {
+        this.tiempoAdquirido = tiempoAdquirido;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente [nroUsuario=" + nroUsuario + ", nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + ", tiempoAdquirido=" + tiempoAdquirido + "]";
+    }
 }
